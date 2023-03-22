1b7afd451c04bb312c15396994c7976f880949ec
beatmil <anutorn@gmail.com>
2023-03-22 23:51:14 +0700

    Update spawner; spawn enemy from Array

--- Scripts/Forest.gd
+++ Scripts/Forest.gd
@@ -1,26 +1,41 @@
 extends Node
 
 
+# Preloads
 var SLIME = preload("res://Nodes/Slime.tscn")
-# var enemy_spawn_order = [bob1, bob2]
 
 
-func _ready():
-	# spawn_slime()
-	spawn_speed_slime()
+# Configs
+var enemy_spawn_order: Array = [spawn_slime, spawn_slime, spawn_speed_slime]
+var order_index: int = 0
 
 
-func spawn_slime():
+# Reference
+@onready var enemy_order_size: int = len(enemy_spawn_order)
+
+
+func _ready() -> void:
+	spawner()
+
+
+func spawner() -> void:
+	enemy_spawn_order[order_index].call()
+	order_index += 1
+	if order_index >= enemy_order_size:
+		order_index = 0
+
+
+func spawn_slime() -> void:
 	var slime = SLIME.instantiate()
 	slime.position = $"Markers/EnemySpawnPos".position
-	slime.connect("ded", spawn_slime)
+	slime.connect("ded", spawner)
 	add_child(slime)
 
 
-func spawn_speed_slime():
+func spawn_speed_slime() -> void:
 	var slime = SLIME.instantiate()
 	slime.speed = Vector2(-1500, 0)
 	slime.activate_speed()
 	slime.position = $"Markers/EnemySpawnPos".position
-	slime.connect("ded", spawn_slime)
+	slime.connect("ded", spawner)
 	add_child(slime)
