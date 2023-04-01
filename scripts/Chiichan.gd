e96af61ba8765d97f774e178ae48ff24daf02594
beatmil <anutorn@gmail.com>
2023-04-01 19:56:40 +0700

    Add movement and jumping to chiichan

--- scripts/Chiichan.gd
+++ scripts/Chiichan.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 
 @export var default_pos: Node
@@ -11,22 +11,44 @@ enum States {RUNNING, ATTACKING, STUNNED, BLOCKING, BLOCK_IMPACT, SWORD_DEFLECT,
 # Configs
 var state = States.RUNNING
 var is_alive = true
-var can_move_to_default_pos = false
-var speed = Vector2(100, 0)
+var speed = 100000
+var jump_power = 900
+var gravity_power = 30
 
 
 # Constants
 @onready var anim_player = $AnimationPlayer
 var ATTACK01_HITBOX = preload("res://nodes/hitboxes/attack01_hitbox.tscn")
+const FRICTION = 0.1
 
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-func _integrate_forces(_state):
-	if position.x < default_pos.position.x and can_move_to_default_pos and is_alive:
-		linear_velocity = speed
+func _physics_process(delta):
+	if state != States.STUNNED:
+		# Jump
+		if Input.is_action_just_pressed("jump"):
+			if is_on_floor():
+				jump()
+		elif Input.is_action_just_released("jump"):
+			release_jump()
+
+
+		# normal movement
+		if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
+			lerp_velocity_x()
+		elif Input.is_action_pressed("move_left"):
+			velocity = Vector2(-speed * delta, velocity.y)
+		elif Input.is_action_pressed("move_right"):
+			velocity = Vector2(speed * delta, velocity.y)
+		else:
+			lerp_velocity_x()
+
+	# Add gravity and calculate movements
+	gravity()
+	move_and_slide()
 
 
 func _input(event):
@@ -36,7 +58,7 @@ func _input(event):
 	if state == States.STUNNED:
 		return
 
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("attack"):
 		attack01()
 
 	if event.is_action_pressed("block"):
@@ -44,6 +66,10 @@ func _input(event):
 
 	if event.is_action_pressed("dodge"):
 		dodge()
+
+
+func lerp_velocity_x():
+	velocity = velocity.lerp(Vector2(0, velocity.y), FRICTION)
 
 
 func spawn_hitbox01(): # used by AnimationPlayer
@@ -61,26 +87,34 @@ func push(power: Vector2):
 	
 	state = States.STUNNED
 
+	velocity += power
+
 	# Set linear_velocity to ZERO
 	# Make it easier to control how far chiichan would fly 
 	# When she collides while moving and while stay still
-	linear_velocity = Vector2.ZERO
-	apply_impulse(power)
+	# linear_velocity = Vector2.ZERO
+	# apply_impulse(power)
 
-	can_move_to_default_pos = false
+
+func jump():
+	anim_player.play("jump")
+	velocity += Vector2(0, -jump_power)
+
+
+func release_jump():
+	if velocity.y < 0: # while rising
+		velocity = Vector2(velocity.x, 0)
 
 
 func ded():
 	is_alive = false
-	can_move_to_default_pos = false
-	apply_impulse(Vector2(-800, -100))
 	$"%DedMenu".show_menu()
 
 
 func sword_deflect():
 	$"AnimationPlayer".play("sword_deflect")
 	state = States.STUNNED
-	apply_impulse(Vector2(-900, -100))
+	velocity = Vector2.ZERO
 
 
 func attack01():
@@ -97,6 +131,11 @@ func dodge():
 	anim_player.play("dodge")
 
 
+func gravity():
+	if not is_on_floor():
+		velocity += Vector2(0, gravity_power)
+
+
 func dodge_collision():
 	collision_layer = 0b00000000000000001000
 	collision_mask = 0b00000000000000000001
@@ -110,5 +149,4 @@ func normal_collision():
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name in ["attack01", "hurt", "block", "block_impact", "attack01_2", "dodge"]:
 		state = States.RUNNING
-		can_move_to_default_pos = true
 		anim_player.play("run")
