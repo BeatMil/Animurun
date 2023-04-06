8fa7155ace42c9f01feae0622344a3c07e008516
beatmil <anutorn@gmail.com>
2023-04-07 02:55:13 +0700

    Spawner now stop when paused

--- scripts/Forest.gd
+++ scripts/Forest.gd
@@ -8,7 +8,7 @@ var BOMBY = preload("res://nodes/Bomby.tscn")
 
 
 # Configs
-var enemy_spawn_order: Array = [spawn_triple02]
+var enemy_spawn_order: Array = [spawn_2bomb_rock_slime, spawn_five_ground_bombs]
 var order_index: int = 0
 
 
@@ -88,11 +88,11 @@ func spawn_triple01() -> void:
 	# add bomby
 	bomby.throw_bomb(Vector2(-2000, -2000))
 	add_child(bomby)
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	# add rocky
 	add_child(rocky)
-	await get_tree().create_timer(0.5).timeout
+	await get_tree().create_timer(0.5, false).timeout
 
 	# add slime
 	add_child(slime)
@@ -111,11 +111,11 @@ func spawn_triple02() -> void:
 
 	add_child(slime1)
 	slime1.throw_slime(Vector2(-2000, -2500))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(slime2)
 	slime2.throw_slime(Vector2(-2000, -2000))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(slime3)
 	slime3.throw_slime(Vector2(-1900, -1900))
@@ -134,11 +134,11 @@ func spawn_triple03() -> void:
 
 	add_child(node1)
 	node1.throw_bomb(Vector2(-2000, -2500))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node2)
 	node2.throw_bomb(Vector2(-2000, -2000))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node3)
 	node3.throw_bomb(Vector2(-1900, -1900))
@@ -157,11 +157,11 @@ func spawn_triple04() -> void:
 
 	add_child(node1)
 	node1.throw_bomb(Vector2(-1500, -1500))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node2)
 	node2.throw_bomb(Vector2(-1300, -1300))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node3)
 	node3.throw_bomb(Vector2(-1000, -1000))
@@ -176,7 +176,7 @@ func spawn_triple05() -> void:
 	slime3.position = $"Markers/EnemySpawnPos".position
 
 	add_child(slime1)
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.1, false).timeout
 
 	add_child(slime3)
 
@@ -200,23 +200,55 @@ func spawn_five_bombs() -> void:
 
 	add_child(node1)
 	node1.throw_bomb(Vector2(-1000, -1000))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node2)
 	node2.throw_bomb(Vector2(-1300, -1300))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node3)
 	node3.throw_bomb(Vector2(-1500, -1500))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node4)
 	node4.throw_bomb(Vector2(-1900, -1900))
-	await get_tree().create_timer(0.3).timeout
+	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(node5)
-	node5.throw_bomb(Vector2(-2100, -2100))
-	await get_tree().create_timer(0.3).timeout
+	node5.throw_bomb(Vector2(-2300, -2300))
+	await get_tree().create_timer(0.3, false).timeout
+
+
+func spawn_five_ground_bombs() -> void:
+	var node1 = BOMBY.instantiate()
+	node1.position = $"Markers/EnemySpawnPos".position
+
+	var node2 = BOMBY.instantiate()
+	node2.position = $"Markers/EnemySpawnPos".position
+
+	var node3 = BOMBY.instantiate()
+	node3.position = $"Markers/EnemySpawnPos".position
+
+	var node4 = BOMBY.instantiate()
+	node4.position = $"Markers/EnemySpawnPos".position
+
+	var node5 = BOMBY.instantiate()
+	node5.connect("ded", spawner)
+	node5.position = $"Markers/EnemySpawnPos".position
+
+	add_child(node1)
+	await get_tree().create_timer(0.2, false).timeout
+
+	add_child(node2)
+	await get_tree().create_timer(0.2, false).timeout
+
+	add_child(node3)
+	await get_tree().create_timer(0.2, false).timeout
+
+	add_child(node4)
+	await get_tree().create_timer(0.2, false).timeout
+
+	add_child(node5)
 
 
 func spawn_2rock_1speed() -> void:
@@ -232,12 +264,36 @@ func spawn_2rock_1speed() -> void:
 	bomby.position = $"Markers/EnemySpawnPos".position
 
 	add_child(bomby)
-	await get_tree().create_timer(0.1).timeout
+	await get_tree().create_timer(0.1, false).timeout
 
 	add_child(rocky)
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(0.2, false).timeout
 
 	add_child(rocky2)
+
+
+func spawn_2bomb_rock_slime() -> void:
+	var bomby1 = BOMBY.instantiate()
+	bomby1.position = $"Markers/EnemySpawnPos".position
+
+	var bomby2 = BOMBY.instantiate()
+	bomby2.position = $"Markers/EnemySpawnPos".position
+
+	var rocky = ROCKY.instantiate()
+	rocky.position = $"Markers/EnemySpawnPos".position
+
+	var slime = SLIME.instantiate()
+	slime.connect("ded", spawner)
+	slime.position = $"Markers/EnemySpawnPos".position
+
+	add_child(bomby1)
+	await get_tree().create_timer(1, false).timeout
+
+
+	add_child(rocky)
+	await get_tree().create_timer(0.3, false).timeout
+
+	add_child(slime)
 
 
 func _on_spawn_timer_timeout():
