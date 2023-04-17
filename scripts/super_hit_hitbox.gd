73868e9b8ac2e09d1c338bdaac66527fea3d30d3
beatmil <anutorn@gmail.com>
2023-04-17 17:30:32 +0700

    Add hit_spark
    Looking gud XD

--- scripts/super_hit_hitbox.gd
+++ scripts/super_hit_hitbox.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
 
+# preloads
+var HIT_SPARK = preload("res://nodes/particles/hit_spark.tscn")
+
+
+func _spawn_hit_spark(_node_pos: Node) -> void:
+	var hitbox = HIT_SPARK.instantiate()
+	hitbox.position = _node_pos.position
+	$"..".add_child(hitbox)
+
+
 func _on_body_entered(body):
 	if body.is_in_group("slime"):
 		body.activate_boom_then()
+		_spawn_hit_spark(body)
 		$"../CameraWrap/CameraPlayer".play("smol_shake_2")
 
 
