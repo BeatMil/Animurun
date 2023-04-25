df804b5e84e96cd7042766602df64b4fff9685f3
beatmil <anutorn@gmail.com>
2023-04-26 04:15:29 +0700

    In progress of adding spike

---
+++ scripts/spike.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_body_entered(body):
+	if body.is_in_group("chiichan"):
+		body.set_state(body.States.RUNNING) # chiichan can't parry this
+		body.push(Vector2(-2000, -1000))
+
+
+func _on_timer_timeout():
+	$AnimationPlayer.play("spike")
