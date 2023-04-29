68eb2499c3db0396ae9dc368e0810eed4b7045ae
chiichan <anutorn@gmail.com>
2023-04-29 18:00:20 +0700

    Move some enemy_pattern to new file

--- scripts/forest.gd
+++ scripts/forest.gd
@@ -17,7 +17,7 @@ var enemy_spawn_order: Array = [spawn_slime, spawn_bomby]
 var order_index: int = 0 # spawner helper
 var is_random_spawn = false
 var rng = RandomNumberGenerator.new()
-var tutorial_phase_helper = 3
+var tutorial_phase_helper = 1
 
 
 # Reference
@@ -57,10 +57,13 @@ func spawn_tutorial_phase() -> void:
 
 
 func spawn_phase_one() -> void:
-	enemy_spawn_order = [spawn_triple_slime, spawn_triple_slime]
+	enemy_spawn_order = [spawn_parry_dodge_chain, spawn_two_slime]
 	enemy_order_size = enemy_spawn_order.size()
 
 
+###
+### Enemy Patterns
+### Starts
 func spawn_slime() -> void:
 	var slime = SLIME.instantiate()
 	slime.position = $"Markers/EnemySpawnPos".position
@@ -98,58 +101,38 @@ func spawn_speed_bomby() -> void:
 	add_child(bomby)
 
 
-func spawn_spike() -> void:
-	var spike = SPIKE.instantiate()
-	spike.position = $"Markers/SpikeSpawnPos".position
-	spike.connect("ded", spawner)
-	add_child(spike)
-
-
-func spawn_triple01() -> void:
-	var bomby = BOMBY.instantiate()
-	bomby.position = $"Markers/EnemySpawnPos2".position
-
-	var rocky = ROCKY.instantiate()
-	rocky.position = $"Markers/EnemySpawnPos".position
-
-	var slime = SLIME.instantiate()
-	slime.connect("ded", spawner)
-	slime.position = $"Markers/EnemySpawnPos".position
-
-	# add bomby
-	bomby.throw_bomb(Vector2(-2000, -2000))
-	add_child(bomby)
-	await get_tree().create_timer(0.3, false).timeout
-
-	# add rocky
-	add_child(rocky)
-	await get_tree().create_timer(0.2, false).timeout
-
-	# add slime
-	add_child(slime)
-
-
-func spawn_triple02() -> void:
+func spawn_two_slime() -> void: # learn to chain parry
 	var slime1 = SLIME.instantiate()
 	slime1.position = $"Markers/EnemySpawnPos".position
-
-	var slime2 = SLIME.instantiate()
-	slime2.position = $"Markers/EnemySpawnPos".position
 
 	var slime3 = SLIME.instantiate()
 	slime3.connect("ded", spawner)
 	slime3.position = $"Markers/EnemySpawnPos".position
 
 	add_child(slime1)
-	slime1.throw_slime(Vector2(-2000, -2500))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(slime2)
-	slime2.throw_slime(Vector2(-2000, -2000))
 	await get_tree().create_timer(0.3, false).timeout
 
 	add_child(slime3)
-	slime3.throw_slime(Vector2(-1900, -1900))
+
+
+func spawn_parry_dodge_chain() -> void: # learn to chain parry and dodge
+	var slime1 = SLIME.instantiate()
+	slime1.position = $"Markers/EnemySpawnPos".position
+
+	var bomby = BOMBY.instantiate()
+	bomby.position = $"Markers/EnemySpawnPos".position
+
+	var slime3 = SLIME.instantiate()
+	slime3.connect("ded", spawner)
+	slime3.position = $"Markers/EnemySpawnPos".position
+
+	add_child(slime1)
+	await get_tree().create_timer(0.3, false).timeout
+
+	add_child(slime3)
+	await get_tree().create_timer(0.3, false).timeout
+
+	add_child(bomby)
 
 
 func spawn_triple_slime() -> void:
@@ -175,235 +158,11 @@ func spawn_triple_slime() -> void:
 	slime3.throw_slime(Vector2(-2000, -2000))
 
 
-func spawn_triple03() -> void:
-	var node1 = BOMBY.instantiate()
-	node1.position = $"Markers/EnemySpawnPos2".position
-
-	var node2 = BOMBY.instantiate()
-	node2.position = $"Markers/EnemySpawnPos2".position
-
-	var node3 = BOMBY.instantiate()
-	node3.connect("ded", spawner)
-	node3.position = $"Markers/EnemySpawnPos2".position
-
-	add_child(node1)
-	node1.throw_bomb(Vector2(-2000, -2500))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node2)
-	node2.throw_bomb(Vector2(-2000, -2000))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node3)
-	node3.throw_bomb(Vector2(-1900, -1900))
-
-
-func spawn_triple04() -> void:
-	var node1 = BOMBY.instantiate()
-	node1.position = $"Markers/EnemySpawnPos2".position
-
-	var node2 = BOMBY.instantiate()
-	node2.position = $"Markers/EnemySpawnPos2".position
-
-	var node3 = BOMBY.instantiate()
-	node3.connect("ded", spawner)
-	node3.position = $"Markers/EnemySpawnPos2".position
-
-	add_child(node3)
-	node3.throw_bomb(Vector2(-1000, -1000))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node2)
-	node2.throw_bomb(Vector2(-1900, -1900))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node1)
-	node1.throw_bomb(Vector2(-2200, -2200))
-
-
-func spawn_triple05() -> void:
-	var slime1 = SLIME.instantiate()
-	slime1.position = $"Markers/EnemySpawnPos".position
-
-	var slime3 = SLIME.instantiate()
-	slime3.connect("ded", spawner)
-	slime3.position = $"Markers/EnemySpawnPos".position
-
-	add_child(slime1)
-	await get_tree().create_timer(0.1, false).timeout
-
-	add_child(slime3)
-
-
-func spawn_five_bombs() -> void:
-	var node1 = BOMBY.instantiate()
-	node1.position = $"Markers/EnemySpawnPos2".position
-
-	var node2 = BOMBY.instantiate()
-	node2.position = $"Markers/EnemySpawnPos2".position
-
-	var node3 = BOMBY.instantiate()
-	node3.position = $"Markers/EnemySpawnPos2".position
-
-	var node4 = BOMBY.instantiate()
-	node4.position = $"Markers/EnemySpawnPos2".position
-
-	var node5 = BOMBY.instantiate()
-	node5.connect("ded", spawner)
-	node5.position = $"Markers/EnemySpawnPos2".position
-
-	add_child(node1)
-	node1.throw_bomb(Vector2(-1000, -1000))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node2)
-	node2.throw_bomb(Vector2(-1300, -1300))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node3)
-	node3.throw_bomb(Vector2(-1500, -1500))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node4)
-	node4.throw_bomb(Vector2(-1900, -1900))
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(node5)
-	node5.throw_bomb(Vector2(-2300, -2300))
-	await get_tree().create_timer(0.3, false).timeout
-
-
-func spawn_five_ground_bombs() -> void:
-	var node1 = BOMBY.instantiate()
-	node1.position = $"Markers/EnemySpawnPos".position
-
-	var node2 = BOMBY.instantiate()
-	node2.position = $"Markers/EnemySpawnPos".position
-
-	var node3 = BOMBY.instantiate()
-	node3.position = $"Markers/EnemySpawnPos".position
-
-	var node4 = BOMBY.instantiate()
-	node4.position = $"Markers/EnemySpawnPos".position
-
-	var node5 = BOMBY.instantiate()
-	node5.connect("ded", spawner)
-	node5.position = $"Markers/EnemySpawnPos".position
-
-	add_child(node1)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(node2)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(node3)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(node4)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(node5)
-
-
-func spawn_2rock_1speed() -> void:
-	var rocky = ROCKY.instantiate()
-	rocky.position = $"Markers/EnemySpawnPos".position
-
-	var rocky2 = ROCKY.instantiate()
-	rocky2.position = $"Markers/EnemySpawnPos".position
-
-	var bomby = BOMBY.instantiate()
-	bomby.activate_speed()
-	bomby.connect("ded", spawner)
-	bomby.position = $"Markers/EnemySpawnPos".position
-
-	add_child(bomby)
-	await get_tree().create_timer(0.1, false).timeout
-
-	add_child(rocky)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(rocky2)
-
-
-func spawn_2bomb_rock_slime() -> void:
-	var bomby1 = BOMBY.instantiate()
-	bomby1.position = $"Markers/EnemySpawnPos".position
-
-	var bomby2 = BOMBY.instantiate()
-	bomby2.position = $"Markers/EnemySpawnPos".position
-
-	var rocky = ROCKY.instantiate()
-	rocky.position = $"Markers/EnemySpawnPos".position
-
-	var slime = SLIME.instantiate()
-	slime.connect("ded", spawner)
-	slime.position = $"Markers/EnemySpawnPos".position
-
-	add_child(bomby1)
-	await get_tree().create_timer(1, false).timeout
-
-	add_child(rocky)
-	await get_tree().create_timer(0.3, false).timeout
-
-	add_child(slime)
-
-
-func spawn_slime_rocks() -> void:
-	var rocky = ROCKY.instantiate()
-	rocky.position = $"Markers/EnemySpawnPos".position
-
-	var rocky2 = ROCKY.instantiate()
-	rocky2.connect("ded", spawner)
-	rocky2.position = $"Markers/EnemySpawnPos".position
-
-	var slime = SLIME.instantiate()
-	slime.position = $"Markers/EnemySpawnPos".position
-
-	var slime2 = SLIME.instantiate()
-	slime2.position = $"Markers/EnemySpawnPos".position
-
-	add_child(slime)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(rocky)
-	await get_tree().create_timer(0.8, false).timeout
-
-	add_child(slime2)
-	await get_tree().create_timer(0.2, false).timeout
-
-	add_child(rocky2)
-
-
-func spawn_2rock_1slime() -> void:
-	var rocky = ROCKY.instantiate()
-	rocky.position = $"Markers/EnemySpawnPos".position
-
-	var rocky2 = ROCKY.instantiate()
-	rocky2.position = $"Markers/EnemySpawnPos".position
-
-	var slime = SLIME.instantiate()
-	slime.connect("ded", spawner)
-	slime.position = $"Markers/EnemySpawnPos".position
-
-	add_child(rocky)
-	await get_tree().create_timer(0.1, false).timeout
-
-	add_child(rocky2)
-	await get_tree().create_timer(0.5, false).timeout
-
-	add_child(slime)
-	slime.activate_speed()
-
-
-func spawn_boom_slime_sword() -> void:
-	var slime = SLIME.instantiate()
-	slime.is_boom_slime = true
-	slime.connect("ded", spawner)
-	slime.position = $"Markers/EnemySpawnPos2".position
-
-	add_child(slime)
-	slime.throw_slime(Vector2(-3000, -2700))
+func spawn_spike() -> void:
+	var spike = SPIKE.instantiate()
+	spike.position = $"Markers/SpikeSpawnPos".position
+	spike.connect("ded", spawner)
+	add_child(spike)
 
 
 func spawn_boom_slime_hand() -> void:
@@ -414,6 +173,10 @@ func spawn_boom_slime_hand() -> void:
 
 	add_child(slime)
 	slime.throw_slime(Vector2(-3000, -1200))
+
+###
+### Enemy Patterns
+### Ends
 
 
 func smol_shake() -> void:
