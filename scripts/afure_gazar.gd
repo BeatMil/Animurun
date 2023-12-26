38e1c0d47cf59a381b6afc9b036f011a6d905a31
beatmil <anutorn@gmail.com>
2023-12-26 19:18:00 +0000

    Adjust afure_gazar to unblockable

--- scripts/afure_gazar.gd
+++ scripts/afure_gazar.gd
@@ -10,7 +10,8 @@ var HIT_SPARK = preload("res://nodes/particles/hit_spark.tscn")
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("chiichan"):
-		body.push(Vector2(-2000, 0))
+		body.set_state(body.States.RUNNING) # chiichan can't parry this
+		body.push(Vector2(-9000, -1900))
 
 
 func call_camera_shake():
@@ -20,6 +21,10 @@ func call_camera_shake():
 func _queue_free():
 	emit_signal("ded")
 	queue_free()
+
+
+func _turn_off_area2d():
+	$Area2D.set_deferred("monitoring", false)
 
 
 func _spawn_hit_spark() -> void:
