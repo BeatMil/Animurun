4a0bc0a6f991d1e610b77e398ff0cc5df49f3888
beatmil <anutorn@gmail.com>
2024-01-03 21:05:19 +0000

    Add scene_transition

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -8,7 +8,7 @@ var current_pic_index = 0
 func _input(_event):
 	if Input.is_action_just_pressed("ui_accept"):
 		if current_pic_index >= intro_pics.size() - 1:
-			get_tree().change_scene_to_file("res://scenes/forest.tscn")
+			SceneTransition.change_scene("res://scenes/forest.tscn")
 			return
 		current_pic_index += 1
 		$AnimationPlayer.play(intro_pics[current_pic_index])
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/intro.tscn")
+	SceneTransition.change_scene("res://scenes/intro.tscn")
