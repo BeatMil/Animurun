6b990e3bfeb7aae14ae879f87bc6473f413d13ca
beatmil <anutorn@gmail.com>
2024-01-15 21:35:01 +0000

    Fix final_boss attack animation freeze

--- scripts/FinalBoss.gd
+++ scripts/FinalBoss.gd
@@ -3,3 +3,8 @@ extends Sprite2D
 
 func play_attack():
 	$AnimationPlayer.play("attack")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "attack":
+		$AnimationPlayer.play("RESET")
