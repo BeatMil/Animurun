5935fbb8786c2e13183ae210724e7a7bf69fc74f
beatmil <anutorn@gmail.com>
2024-02-22 19:44:44 +0000

    Update main_menu buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,22 @@ extends Control
 
 
 func _ready():
-	$HBoxContainer/VBoxContainer/Button.grab_focus()
+#	$HBoxContainer/VBoxContainer/Button.grab_focus()
+	pass
 
 
-func _on_button_pressed():
+
+func _on_start_button_pressed():
 	SceneTransition.change_scene("res://scenes/intro.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
+
+
+func _on_option_button_pressed():
+	$OptionMenu.visible = true
+
+
+func _on_back_button_pressed():
+	$OptionMenu.visible = false
