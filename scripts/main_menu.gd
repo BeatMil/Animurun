1613b376bf66571a1264f2ef32307562ade83c2a
beatmil <anutorn@gmail.com>
2024-03-03 21:45:18 +0000

    Add stage buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -22,3 +22,15 @@ func _on_option_button_pressed():
 
 func _on_back_button_pressed():
 	$OptionMenu.visible = false
+
+
+func _on_stage_button_pressed():
+	SceneTransition.change_scene("res://scenes/ice_mountain.tscn")
+
+
+func _on_stage_button_2_pressed():
+	SceneTransition.change_scene("res://scenes/volcano.tscn")
+
+
+func _on_stage_button_3_pressed():
+	SceneTransition.change_scene("res://scenes/castle.tscn")
