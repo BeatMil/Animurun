bcc19cb5d8f41dd8dd7305ae25e690dabda4ee94
beatmil <anutorn@gmail.com>
2024-03-18 01:59:42 +0000

    Add keybind to option
    Very nice~  XD

---
+++ scripts/option_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+var is_jump_key_change: bool = false
+
+
+func _input(event):
+	if event.is_pressed() and is_jump_key_change:
+		InputMap.action_erase_events("jump")
+		InputMap.action_add_event("jump", event)
+		is_jump_key_change = false
+		$ColorRect2/Button/JumpKeyLabel.text = event.as_text()
+
+
+func _on_button_pressed():
+	$ColorRect2/Button/JumpKeyLabel.text = "?"
+	is_jump_key_change = true
