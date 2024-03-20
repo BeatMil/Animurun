7cb1ca845135d1fdc07f20eff6948e4c17d37fd3
beatmil <anutorn@gmail.com>
2024-03-20 01:16:27 +0000

    Add ability to bind block button

--- scripts/option_menu.gd
+++ scripts/option_menu.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 
 var is_jump_key_change: bool = false
+var is_block_key_change: bool = false
 
 
 func _input(event):
@@ -11,8 +12,19 @@ func _input(event):
 		is_jump_key_change = false
 		$ColorRect2/Button/JumpKeyLabel.text = event.as_text()
 		$ColorRect2/Button.release_focus()
+	elif event.is_pressed() and is_block_key_change:
+		InputMap.action_erase_events("block")
+		InputMap.action_add_event("block", event)
+		is_block_key_change = false
+		$ColorRect2/BlockButton/KeyLabel.text = event.as_text()
+		$ColorRect2/Button.release_focus()
 
 
 func _on_button_pressed():
 	$ColorRect2/Button/JumpKeyLabel.text = "?"
 	is_jump_key_change = true
+
+
+func _on_parry_button_pressed():
+	$ColorRect2/BlockButton/KeyLabel.text = "?"
+	is_block_key_change = true
